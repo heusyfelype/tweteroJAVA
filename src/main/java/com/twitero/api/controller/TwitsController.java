7b5c9725fec0e449fa34f3c8f5ed2633b6a722cf
heusyfelype <felype.heusy@gmail.com>
2023-01-23 23:36:34 -0300

    feat: post tweets

--- src/main/java/com/twitero/api/controller/TwitsController.java
+++ src/main/java/com/twitero/api/controller/TwitsController.java
@@ -1,10 +1,14 @@
 package com.twitero.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.twitero.api.dto.TweetDTO;
@@ -18,7 +22,8 @@ public class TwitsController {
   TweetsService service;
 
   @PostMapping
-  public void create(@RequestBody TweetDTO req){
+  public ResponseEntity<String> create(@RequestBody TweetDTO req) {
     service.create(req);
+    return ResponseEntity.ok().body("ok");
   }
 }
